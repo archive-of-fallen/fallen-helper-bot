bb8a51093e3868c688388f29ea564cbc8d3bfe39
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-12 21:19:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ client.on('ready', () => {
 let prefix = "++"
 
 client.on('message', msg => {  
-  if (msg.guild.members.filter(members => members.displayName.startsWith("!"))) {
-    members.setNickname(`${members.displayName.substr(2, 100)}`, `Changed ${members.displayName}#${members.user.tag}'s nickname for hoisting.`)
+  if (msg.guild.members.filter(mem => mem.username.startsWith("!"))) {
+    mem.setNickname(`${mem.username.substr(2, 100)}`, `Changed ${mem.username}#${mem.user.tag}'s nickname for hoisting.`)
   }
   
   if (msg.content.startsWith(prefix + 'ping')) {
