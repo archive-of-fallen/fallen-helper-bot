a9345225884b615979d34e9ebcdc3568190145d2
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-12 16:07:51 +0800

    Boilerplate.

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`[${new Date()}] Successfully logged in as ${client.user.tag}!`);
+  client.user.setGame("H-hey? Will you be my friend...?")
+});
+
+let prefix = "++"
+
+client.on('message', msg => {
+  if (msg.content.startsWith(prefix + 'ping')) {
+    msg.reply('Why are you interested in checking out my response time?').then(sentMsg => {
+        sentMsg.edit(`Pong! [${sentMsg.createdTimestamp - msg.createdTimestamp}ms]`)
+      })
+  }
+});
+
+client.login(process.env.token);
