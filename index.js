472b547054995c617a057bae4f3a234bcca4a892
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-12 16:27:49 +0800

    Update.

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ client.on('ready', () => {
 let prefix = "++"
 
 client.on('message', msg => {
-  if (msg.guild.member.nickname.startsWith("!")).then(nickname => msg.member.setNickname("No hoists please"))
+  if (msg.guild.member.nickname.startsWith("!")) {
+    msg.member.setNickname(`${msg.member.username.substr(2, 100)}`, `This user was hoisting so I changed their nickname!`)
+  }
   
   if (msg.content.startsWith(prefix + 'ping')) {
     msg.reply('Why are you interested in checking out my response time?').then(sentMsg => {
