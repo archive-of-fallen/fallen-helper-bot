32b14fa1caf2e80db106fbdec9d166a4965c6e08
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-13 14:39:33 +0800

    Forgot clean function.

--- index.js
+++ index.js
@@ -41,4 +41,11 @@ client.on('message', msg => {
     }
 });
 
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
 client.login(process.env.token);
