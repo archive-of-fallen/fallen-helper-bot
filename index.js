7d45f9ba25f05ed56a412c8d5dfe13408db34820
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-13 18:18:56 +0800

    Change date format lol

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`[${new Date().toString().substr(0, 23)}] Successfully logged in as ${client.user.tag}!`);
+  console.log(`[${new Date().toString().substr(16, 23)}] Successfully logged in as ${client.user.tag}!`);
   client.user.setGame("You do not need to know my purpose.")
 });
 
