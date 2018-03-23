dc83d0628692abbd20903f0856e11d3c0a63f287
FallenLight <xFallenLight@users.noreply.github.com>
2018-03-23 15:41:14 +0800

    Fixes to indentation.

--- index.js
+++ index.js
@@ -11,52 +11,48 @@ const prefix = "++"
 const authorizedUsers = [ "300992784020668416", "299175087389802496", "298706728856453121" ]
 
 client.on('message', msg => { 
-  msg.guild.members.filter(
-    mem => {
+  msg.guild.members.filter(mem => {
       mem.displayName.startsWith("!") && 
-      mem.setNickname(`${mem.displayName.replace(/^(\s|!)+/,"") || mem.user.username.replace(/^(\s|!)+/,"") || "."}`)
-    }
-  ) 
+      mem.setNickname(`${mem.displayName.replace(/^(\s|!)+/,"") || mem.user.username.replace(/^(\s|!)+/,"") || "."}`);
+    }); 
 
   if (msg.content.startsWith(prefix + 'ping')) {
     msg.reply('Why are you interested in checking out my response time?').then(sentMsg => {
-        sentMsg.edit(`Pong! [${sentMsg.createdTimestamp - msg.createdTimestamp}ms]`)
-      })
+      sentMsg.edit(`Pong! [${sentMsg.createdTimestamp - msg.createdTimestamp}ms]`);
+    });
   }
   
   if (msg.content.startsWith(prefix + 'eval')) {
     // if (msg.author.id !== "300992784020668416") return;
     if (!authorizedUsers.includes(msg.author.id)) return;
-    let evall = msg.content.split(' ')[0];
-    let evalstuff = msg.content.split(" ").slice(1).join(" ");
     try {
       const code = msg.content.split(" ").slice(1).join(" ");
       let evaled = eval(code);
       if (!code) {
             return msg.channel.send("Please provide something for me to eval!");
-         }
+      }
     
       if (typeof evaled !== 'string')
         evaled = require('util').inspect(evaled);
         
-         msg.channel.send("Evaluating, please wait...").then(sentMsg => {
-           msg.channel.send('📤 Success! Here is your output:');
-           sentMsg.edit(`${clean(evaled)}`, { code: 'js' });
-         });
-      } catch (err) {
-         msg.channel.send("Evaluating, please wait...").then(sentMsg => {
-           msg.channel.send('📤 Oh no, an error occurred! Here is your output:');
-           sentMsg.edit(`${clean(err)}`, { code: 'js' });
-         });
-      }
+        msg.channel.send("Evaluating, please wait...").then(sentMsg => {
+          msg.channel.send('📤 Success! Here is your output:');
+          sentMsg.edit(`${clean(evaled)}`, { code: 'js' });
+        });
+    } catch(err) {
+        msg.channel.send("Evaluating, please wait...").then(sentMsg => {
+          msg.channel.send('📤 Oh no, an error occurred! Here is your output:');
+          sentMsg.edit(`${clean(err)}`, { code: 'js' });
+        });
+    }
   }
 });
 
 function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+    return text;
 }
 
 client.login(process.env.token);
